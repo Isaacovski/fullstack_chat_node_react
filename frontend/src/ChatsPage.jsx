7d1d818a7894047583a9a5c18c31434e7fc3c59e
Isaac Gabriel <codebiisaac@gmail.com>
2024-06-08 18:26:22 -0300

    projeto totalmente finalizado e funcional

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats . . .</>
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{height:'100vh'}}>
+        <PrettyChatWindow
+        projectId='a52e55ae-b89c-4401-a3d0-556d65dc8cd4'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{height: '100%'}}
+        />
+    </div>
+    )
 }
 
 export default ChatsPage
