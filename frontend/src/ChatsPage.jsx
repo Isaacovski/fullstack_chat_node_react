ab0a66961b4b6ba92b803899b1215303376f0fb0
Isaacovski <codebiisaac@gmail.com>
2024-06-24 14:35:15 -0300

    pronto para usar

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div style={{height:'100vh'}}>
         <PrettyChatWindow
-        projectId='a52e55ae-b89c-4401-a3d0-556d65dc8cd4'
+        projectId='2a9ad344-208d-4555-acd0-3bddcdcebc32'
         username={props.user.username}
         secret={props.user.secret}
         style={{height: '100%'}}
